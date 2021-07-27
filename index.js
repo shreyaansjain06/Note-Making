16beebdba5d9df05afef8792a44dd5d0065e7911
Shreyaans Jain <shreyaansjain60@gmail.com>
2021-07-27 13:01:59 +0530

    add some changes for linking of ejs file

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const express = require('express')
 const app = express()
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static('assets'))
 app.get("/", (req, res) => {
     return res.render("index");
+  });
+  app.get("/login", (req, res) => {
+    return res.render("login");
+  });
+  app.get("/notes", (req, res) => {
+    return res.render("notes");
+  });
+  app.get("/sign", (req, res) => {
+    return res.render("sign");
   });
 app.listen(port, () => {
     console.log('Server is listening on port',port)
